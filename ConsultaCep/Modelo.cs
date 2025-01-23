9b31282d746dbf1db9df96c97daec38599f8462b
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-01-22 22:36:13 -0300

    Implementação da Consulta

---
+++ ConsultaCep/Modelo.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsultaCep.Modelo
+{
+    internal class Cep
+    {
+        [JsonPropertyName("logradouro")]
+        public string? Logradouro { get; set; }
+        [JsonPropertyName("bairro")]
+        public string? Bairro { get; set; }
+        [JsonPropertyName("localidade")]
+        public string? Localidade { get; set; }
+        [JsonPropertyName("uf")]
+        public string? Uf { get; set; }
+
+        public Cep(string logradouro, string bairro, string localidade, string uf)
+        {
+            Logradouro = logradouro;
+            Bairro = bairro;
+            Localidade = localidade;
+            Uf = uf;
+        }
+
+        public void ExibirCep()
+        {
+            Console.WriteLine($"Logradouro: {Logradouro}");
+            Console.WriteLine($"Bairro: {Bairro}");
+            Console.WriteLine($"Cidade: {Localidade}");
+            Console.WriteLine($"UF: {Uf}");
+        }
+    }
+}
--- ConsultaCep/Program.cs
+++ ConsultaCep/Program.cs
@@ -1,4 +1,19 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using System.Text.Json;
+using ConsultaCep.Modelo;
+
+using (HttpClient client = new HttpClient())
 {
-    
+    try
+    {
+        int cep = 29124337;
+
+        string resposta = await client.GetStringAsync($"https://viacep.com.br/ws/{cep}/json/");
+        Cep endereco = JsonSerializer.Deserialize<Cep>(resposta);
+        endereco.ExibirCep();
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Um erro foi encontrado: {ex.Message}");
+    }
 }
